9e03c9802445e3f0b6b3b228461a4b6f68c77883
ryantheengineer <ryan.larson.wa@gmail.com>
2020-04-07 09:22:09 -0600

    Added optimize_fourbar

--- get_potential_energy.m
+++ get_potential_energy.m
@@ -1,11 +1,15 @@
-function [V] = get_potential_energy(r1,r2,r3,r4,theta2,theta20,theta30,theta40,K2,K3)
+function [V] = get_potential_energy(r1,r2,r3,r4,theta2,theta20,theta30,K2,K3)
 
+% Section for theta2 vector
 delta = sqrt(r1^2 + r2^2 - 2.*r1.*r2.*cos(theta2));
 beta = acos((r1^2 + delta.^2 - r2^2)./(2.*r1.*delta));
 psi = acos((r3^2 + delta.^2 - r4^2)./(2.*r3.*delta));
 lamda = acos((r4^2 + delta.^2 - r3^2)./(2.*r4.*delta));
 theta3 = psi - beta;
 theta4 = pi - lamda - beta;
+
+theta40 = theta4(1);
+
 
 
 % psi1 = theta2 - theta20;
