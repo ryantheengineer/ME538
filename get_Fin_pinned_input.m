b9225e294c198a16c13b241310ce0298baebe07d
ryantheengineer <ryan.larson.wa@gmail.com>
2020-04-08 14:37:52 -0600

    Updated get_Fin_pinned_input.m but doesn't yet take into account the M2 moment imposed by the umbrella members

--- get_Fin_pinned_input.m
+++ get_Fin_pinned_input.m
@@ -14,10 +14,14 @@ theta5 = asin((r2.*cos(theta2) + a3.*cos(theta3) - b3.*sin(theta3) + dy)/r5);
 
 T2 = K2.*(theta2 - theta20);
 T3 = K3.*(theta3 - theta30);
+% 
+% Fin = (h32.*(T2+T3) - T2 - T3.*h42)./(r2.*sin(theta5).*cos(theta2) - r2.*cos(theta5).*sin(theta2)...
+%     -a3.*h32.*cos(theta5).*sin(theta3) - b3.*h32.*sin(theta5).*sin(theta3)...
+%     - b3.*h32.*cos(theta5).*cos(theta3) - a3.*sin(theta5).*cos(theta5));
 
-Fin = (h32.*(T2+T3) - T2 - T3.*h42)./(r2.*sin(theta5).*cos(theta2) - r2.*cos(theta5).*sin(theta2)...
-    -a3.*h32.*cos(theta5).*sin(theta3) - b3.*h32.*sin(theta5).*sin(theta3)...
-    - b3.*h32.*cos(theta5).*cos(theta3) - a3.*sin(theta5).*cos(theta5));
+M2 = 0; % This will need to be a function dependent upon theta2 in the final version
+
+Fin = (h32.*(T2+T3) - h42.*T3 - M2 - T2)./(r2.*sin(theta2) + h32.*a3.*sin(theta3) + h32.*b3.*cos(theta2));
 
 
 end
--- optimize_fourbar.m
+++ optimize_fourbar.m
@@ -17,21 +17,57 @@ function [xopt, fopt, exitflag, output] = optimize_fourbar()
     function [f, c, ceq] = objcon(x)
         
         %design variables
-        H = x(1);  %height (in)
-        d = x(2);  %diameter (in)
+        r1 = x(1);          % Link 1 length
+        r2 = x(2);          % Link 2 length
+        r3 = x(3);          % Link 3 length
+        r4 = x(4);          % Link 4 length
+        r5 = x(5);          % Link 5 length
+        dy = x(6);          % Offset between link 1 and slider of link 5
+        a3 = x(7);          % Distance to actuation point along link 3
+        b3 = x(8);          % Distance to actuation point perp. to link 3
+        K2 = x(9);          % Link 2 spring constant
+        K3 = x(10);         % Link 3 spring constant
+        theta20 = x(11);    % Resting angle of link 2
+        theta30 = x(12);    % Resting angle of link 3
+        
         
         %other analysis variables
-        P = 66;    %load (lbs)
-        E = 30000; %modulus of elasticity (lbs/in)
-        rho = 0.3; %density (lbs/in^3)
-        t = .15;   %thickness (in)
-        B = 60;    %separation distance (in)
+        theta2 = linspace(0,pi,200);
+        closeangle = 0;
+        openangle = 80; % Degrees (for simplicity)
+        openangle = openangle*pi/180;
         
-        %analysis functions
-        weight = rho*2*pi*d*t*sqrt((B/2)^2+H^2); %lbs
-        stress = (P*sqrt((B/2)^2+H^2))/(2*t*pi*d*H); %ksi
-        bstress = (pi^2*E*(d^2+t^2))/(8*((B/2)^2+H^2)); %ksi
-        deflection = P*((B/2)^2+H^2)^(3/2)/(2*t*pi*d*H^2*E); %in
+        % Binary indicators of which angle data is within the desired
+        % actuation range. Use as theta2(goodrange) or V(goodrange) later
+        % on. This will allow force searching within only the expected
+        % angles if desired.
+        goodrange = (theta2<openangle);
+        
+        
+        
+        %% Analysis Functions
+        % Potential energy curve
+        V = get_potential_energy(r1,r2,r3,r4,theta2,theta20,theta30,K2,K3);
+        TF = islocalmin(real(V));
+        nmins = nnz(TF);
+        if nmins ~= 2
+            I = zeros(2,1);
+            [min1,I(1)] = min(real(V));
+            I(2) = find(TF);
+            min2 = real(V(I(2)));
+        else
+            I = find(TF);
+            min1 = real(V(I(1)));
+            min2 = real(V(I(2))); 
+        end
+        
+        % Calculate the input force at the 
+        
+%         
+%         weight = rho*2*pi*d*t*sqrt((B/2)^2+H^2); %lbs
+%         stress = (P*sqrt((B/2)^2+H^2))/(2*t*pi*d*H); %ksi
+%         bstress = (pi^2*E*(d^2+t^2))/(8*((B/2)^2+H^2)); %ksi
+%         deflection = P*((B/2)^2+H^2)^(3/2)/(2*t*pi*d*H^2*E); %in
 
         %objective function
         f = weight; %minimize weight
